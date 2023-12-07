cd80e5bd5d7df4e6a1a209a3b8a7db6e234401a3
Flik <2470982985@qq.com>
2023-12-07 21:14:54 +0800

    Update tools.sh

--- src/install/tools.sh
+++ src/install/tools.sh
@@ -6,7 +6,23 @@ echo "Install some common tools for further installation"
 apt-get update
 apt-get install -y git curl vim wget net-tools locales bzip2 procps \
     python3-numpy #used for websockify/novnc
+apt-get install -y language-pack-zh-hans
 apt-get clean -y
 
-echo "generate locales en_US.UTF-8"
-locale-gen en_US.UTF-8
+echo "generate locales zh_CN.UTF-8"
+locale-gen zh_CN.UTF-8
+
+cat > /etc/default/locale << EOF
+LANG="zh_CN.UTF-8"
+LANGUAGE="zh_CN:zh:en_US:en"
+LC_NUMERIC="zh_CN.UTF-8"
+LC_TIME="zh_CN.UTF-8"
+LC_MONETARY="zh_CN.UTF-8"
+LC_PAPER="zh_CN.UTF-8"
+LC_IDENTIFICATION="zh_CN.UTF-8"
+LC_NAME="zh_CN.UTF-8"
+LC_ADDRESS="zh_CN.UTF-8"
+LC_TELEPHONE="zh_CN.UTF-8"
+LC_MEASUREMENT="zh_CN.UTF-8"
+LC_ALL=zh_CN.UTF-8
+EOF
